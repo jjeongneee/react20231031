fd5219b8860c24d6c9a5a4ef7b7ab20017315509
jjeongneee <gardening0530@gmail.com>
2023-10-31 16:42:01 +0900

    스타일 적용 연습, flexbox 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-
 function App() {
+  // const myStyle = {
+  //   display: "flex",
+  //   color: "pink",
+  //   fontSize: "40px",
+  //   textAlign: "center",
+  //   fontWeight: "bold",
+  //   justifyContent: "space-evenly",
+  // };
   return (
-    <div>
-
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <MyComp />
+      <MyComp />
+      <MyComp />
     </div>
   );
 }
 
+function MyComp() {
+  const name = "흥민";
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        padding: "10px",
+        width: "25%",
+        textAlign: "center",
+      }}
+    >
+      {" "}
+      {name}
+    </div>
+  );
+}
 export default App;

5749a6e8cfe007f1e5516194a24559711a1b10c2
jjeongneee <gardening0530@gmail.com>
2023-10-31 12:14:48 +0900

    component 연습, 작성 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+function MyComponent1() {
+    return (
+        <div>
+            <h1>Hello Component</h1>
+        </div>
+    )
+}
+
+function MyComponent2() {
+    return (
+        <>
+            {/*p>lorem5^ul>li*3>lorem1*/}
+            <p>Lorem ipsum dolor sit amet.</p>
+            <ul>
+                <li>Lorem.</li>
+                <li>Libero!</li>
+                <li>Ea!</li>
+            </ul>
+        </>
+    );
+}
 
 function App() {
   return (
     <div>
-
+        <MyComponent1 />
+        {/*
+      <div>
+        <h1>Hello Component</h1>
+      </div>
+      */}
+        <MyComponent2 />
+        {/*
+        <p>lorem ipsum alkdjf alkdjf </p>
+        <ul>
+          <li>lorem</li>
+          <li>ipsum</li>
+          <li>dolor</li>
+        </ul>
+       */}
     </div>
   );
 }

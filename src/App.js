61d67f47d1bbf71935def5fc5c3273fe42eecf9c
jjeongneee <gardening0530@gmail.com>
2023-10-31 14:33:53 +0900

    종료태그 작성연습 완료, 컴포넌트 여러번 사용 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+function MyComponent1() {
+  return (
+    <div style={{ width: "100%" }}>
+      <img
+        style={{ width: "100%" }}
+        src="https://i.imgur.com/SuL4x4v.jpeg"
+        alt="경복궁"
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-
+      <MyComponent1 />
+      <MyComponent1 />
+      <MyComponent1 />
+      {/*
+          <img src="https://imgur.com/a/XWAU1S4" / >
+          <img src="https://imgur.com/a/XWAU1S4" / >
+          <img src="https://imgur.com/a/XWAU1S4" / >
+        */}
     </div>
   );
 }

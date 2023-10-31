9e5a9310b01c4d1f76bcceb1d7807dff3bb6b9a6
jjeongneee <gardening0530@gmail.com>
2023-10-31 15:36:06 +0900

    jsx의 {}에서 간단한 연산자 활용 방법 3가지

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-
 function App() {
+  const num1 = Math.ceil(Math.random() * 6);
+  const num2 = Math.ceil(Math.random() * 6);
+  let message;
+
+  if (num1 === num2) {
+    message = <h1>당첨</h1>;
+  } else {
+    message = <h1>다시 던져 보세요.</h1>;
+  }
+
   return (
     <div>
+      <h1>1번 주사위 : {num1}</h1>
+      <h1>2번 주사위 : {num2}</h1>
 
+      <h1>{num1 === num2 ? "당첨" : "다시 던져 보세요!!!"}</h1>
+      <h1>{num1 === num2 ? <Same /> : <Diff />}</h1>
+
+      <hr />
+
+      {message}
     </div>
   );
+
+  // 두 수를 출력하고
+  // 두 수가 같으면
+  // "당첨!!!" 출력
+  // 두 수가 다르면
+  // "다시 던져 보세요"
+}
+function Same() {
+  return <h1>당첨!!!!</h1>;
+}
+
+function Diff() {
+  return <h1>다시 던져 보세요!!!!</h1>;
 }
 
 export default App;

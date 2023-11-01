3a30ade7cb17208fe003e8362475b268a53988a9
jjeongneee <gardening0530@gmail.com>
2023-11-01 10:17:26 +0900

    props 작성, 활용연습 시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-
 function App() {
   return (
     <div>
+      <MyText
+        style={{
+          color: "pink",
+          backgroundColor: "green",
+          textAlign: "center",
+          border: "5px solid gold",
+        }}
+        value="뭐쓰지"
+      />
+      <MyText
+        style={{
+          color: "yellow",
+          backgroundColor: "purple",
+          textAlign: "center",
+          border: "5px solid gold",
+        }}
+        value="안녕"
+      />
+    </div>
+  );
+}
 
+function MyText(props) {
+  return (
+    <div style={props.style}>
+      <p>{props.value}</p>
     </div>
   );
 }

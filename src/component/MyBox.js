bf90176c194d766f1fc0c2d6d044b58224ef1320
jjeongneee <gardening0530@gmail.com>
2023-11-01 12:22:20 +0900

    한 파일에서 여러개의 값(객체, 값, 함수) export 가능, import 가능

---
+++ src/component/MyBox.js
@@ -0,0 +1,15 @@
+export function MyBox() {
+  return null;
+}
+
+export function MyElem() {
+  return null;
+}
+
+export function MyContainer() {
+  return null;
+}
+// 함수뿐만 아니라 변수도 가능!
+export let name = "손흥민";
+export const address = "신촌";
+export const person = { name: "강인", team: "파리" };
--- src/component/MyHeader.js
+++
@@ -1,7 +0,0 @@
-export function MyHeader() {
-  return (
-    <div>
-      <h1>너무 길다~</h1>
-    </div>
-  );
-}

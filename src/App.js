f56e69778dbb3d4312233551e74e900c9ea36f47
jjeongneee <gardening0530@gmail.com>
2023-11-01 11:15:03 +0900

    props destructuring assignment 구조분해할당 연습시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-
 function App() {
   return (
     <div>
+      {/* MyBox 컴포넌트 2번 사용해보기 */}
+      <MyBox
+        p={"50px"}
+        m={"30px"}
+        bg={"black"}
+        color={"white"}
+        text={"배고프다"}
+      />
+      <MyBox
+        p={"20px"}
+        m={"10px"}
+        bg={"lightgreen"}
+        color={"black"}
+        text={"기운나다"}
+      />
+    </div>
+  );
+}
 
+function MyBox({ p, m, bg, color, text }) {
+  console.log(text, "안나와");
+  return (
+    <div style={{ padding: p, margin: m, color: color, backgroundColor: bg }}>
+      <p>{text}</p>
     </div>
   );
 }

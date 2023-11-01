a336dc19460e5e16001a1f7f97f61499b57056e3
jjeongneee <gardening0530@gmail.com>
2023-11-01 14:27:42 +0900

    default export, import

---
+++ src/component/MyContainer.js
@@ -0,0 +1,12 @@
+function MyBox() {
+  return (
+    <div>
+      <p>뭘 쓸까나</p>
+    </div>
+  );
+}
+
+export const person = { name: "son", address: "seoul" };
+export const team = { title: "득점왕", score: 3 };
+
+export default MyBox;

a641449259afb17e6ad94c8ded590e4bccae7720
jjeongneee <gardening0530@gmail.com>
2023-11-01 12:35:04 +0900

    한 파일에서 여러개의 값(객체, 값, 함수) export 가능, import 가능 연습시작, 선언 따로 export따로 작성 가능

---
+++ src/component/MyElem.js
@@ -0,0 +1,31 @@
+function MyElem() {
+  return (
+    <div>
+      <p>고양이 좋아~</p>
+    </div>
+  );
+}
+
+function MyNavBar() {
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+}
+
+function MyContent() {
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+}
+
+let country = "한국";
+const text = "배가 고프다";
+const value = 100;
+
+export { country, text, value };
+
+export { MyElem, MyNavBar, MyContent };

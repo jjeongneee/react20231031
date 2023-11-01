5068af769723fd9b455197e2ddbbcbae2c77d154
jjeongneee <gardening0530@gmail.com>
2023-11-01 16:15:40 +0900

    보통의 css 파일을 사용할 때 클래스명 중복이 발생할 수 있음

---
+++ src/component/MyBlock.js
@@ -0,0 +1,10 @@
+import "../style/yourStyle.css";
+
+export function MyBlock() {
+  return (
+    <div>
+      {/*h1.error>lorem5*/}
+      <h1 className="error">Lorem ipsum dolor sit amet.</h1>
+    </div>
+  );
+}

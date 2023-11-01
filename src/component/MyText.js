9641ac0656ec9e308943bf4db2ceb856ee7084cc
jjeongneee <gardening0530@gmail.com>
2023-11-01 16:50:27 +0900

    css module을 사용할 때 여러 클래스명을 주는 방법 연습 시작 -> template literal 또는 array.join으로 해결

--- src/component/MyText.js
+++ src/component/MyText.js
@@ -5,7 +5,14 @@ export function MyText() {
       {/* red 배경색, white 글자색 */}
       {/* src/style/mystyle.module.css 파일 생성 */}
       {/* MyText.js에서 mystyle.module.css import 하기 */}
-      <h1 className={css.error}>Lorem ipsum dolor sit amet.</h1>
+      {/* 템플릿 리터럴방법*/}
+      <h1 className={`${css.error} ${css.special} ${css.coffee}`}>
+        Lorem ipsum dolor sit amet.
+      </h1>
+      {/* array join 메서드 쓰는 방법 */}
+      <h1 className={[css.error, css.special, css.coffee].join(" ")}>
+        Lorem ipsum dolor sit amet.
+      </h1>
     </div>
   );
 }

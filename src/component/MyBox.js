a98a8acda6eef75eecb1facad5a03ab9054cab4f
jjeongneee <gardening0530@gmail.com>
2023-11-01 14:17:03 +0900

    default export, import

--- src/component/MyBox.js
+++ src/component/MyBox.js
@@ -1,15 +1,11 @@
-export function MyBox() {
-  return null;
+function MyBox() {
+  return (
+    <div>
+      <h1>THIS IS MYBOX</h1>
+    </div>
+  );
 }
 
-export function MyElem() {
-  return null;
-}
-
-export function MyContainer() {
-  return null;
-}
-// 함수뿐만 아니라 변수도 가능!
-export let name = "손흥민";
-export const address = "신촌";
-export const person = { name: "강인", team: "파리" };
+export const country = "프랑스";
+export const person = { name: "son", age: 33 };
+export default MyBox;

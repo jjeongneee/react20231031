671208feec2a7420b7ebbf5f67fabf0311165c7d
jjeongneee <gardening0530@gmail.com>
2023-11-01 16:15:40 +0900

    사용할 때 클래스명 중복이 발생할 수 있음 -> css module로 중복 방지

---
+++ src/component/MyBlock.js
@@ -0,0 +1,10 @@
+import css from "../style/yourStyle.module.css";
+
+export function MyBlock() {
+  return (
+    <div>
+      {/*h1.error>lorem5*/}
+      <h1 className={css.error}>Lorem ipsum dolor sit amet.</h1>
+    </div>
+  );
+}

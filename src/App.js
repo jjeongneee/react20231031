480d53cfd8d7bc78a707411361632ac3ea2f56f1
jjeongneee <gardening0530@gmail.com>
2023-11-02 15:37:14 +0900

    Image 사용

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import {
-  Box,
-  Flex,
-  FormControl,
-  FormLabel,
-  Switch,
-  Textarea,
-} from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <div>
       <Box w="480px">
-        <FormControl display={"flex"} alignItems={"center"}>
-          <FormLabel mb={0}>알림을 받으시겠습니까?</FormLabel>
-          <Switch size={"lg"} colorScheme="purple" />
-        </FormControl>
+        <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
       </Box>
+      <Box>
+        <Image boxSize={"100px"} src="https://bit.ly/dan-abramov" />
+      </Box>
+      <Box>
+        <Image boxSize={"200px"} src="https://bit.ly/dan-abramov" />
+      </Box>
+      <Box>
+        <Image
+          boxSize={"100px"}
+          borderRadius={"full"}
+          src="https://bit.ly/dan-abramov"
+        />
+      </Box>
+      <Box>
+        <Image src="abcde.jpg" fallbackSrc="https://via.placeholder.com/150" />
+      </Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
     </div>
   );
 }

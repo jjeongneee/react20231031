99ae7a0edf35ab14075494212968344f3e22dfb6
jjeongneee <gardening0530@gmail.com>
2023-11-02 14:34:20 +0900

    Button 사용

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
-import { Button, useToast } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { ArrowRightIcon, EmailIcon } from "@chakra-ui/icons";
 
 function App() {
-  const toast = useToast();
-
   return (
     <>
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장 완료",
-            description: "게시물이 저장되었습니다.",
-            duration: 2000,
-            position: "bottom-left",
-            isClosable: true,
-          })
-        }
-      >
-        토스트 띄우기
+      <Button colorScheme="blue">Lorem.</Button>
+      <Button colorScheme="red">Lorem.</Button>
+      <Button colorScheme="yellow">Natus!</Button>
+      <Button colorScheme="orange">Rerum.</Button>
+      <Button colorScheme="teal" size={"sm"}>
+        Suscipit.
       </Button>
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장 완료",
-            description: "게시물이 저장되지 않았습니다.",
-            duration: 2000,
-            position: "top",
-            isClosable: true,
-            status: "error",
-          })
-        }
-      >
-        토스트 띄우기
+      <Button colorScheme="cyan" size={"lg"}>
+        Itaque.
       </Button>
+      <Button colorScheme="messenger" variant={"outline"}>
+        Veritatis.
+      </Button>
+      <Button colorScheme="orange" variant="ghost">
+        Aliquid!
+      </Button>
+      <Button variant={"link"}>Odit?</Button>
+      <Button leftIcon={<EmailIcon />}>Mollitia.</Button>
+      <Button rightIcon={<ArrowRightIcon />}>Non!</Button>
+      <Button isLoading={true}>Quos.</Button>
+      <Button isLoading loadingText={"전송중"}>
+        Minima.
+      </Button>
+      <Button>Quae!</Button>
+      <Button>Iusto?</Button>
+      <Button>Dolor.</Button>
+      <Button>Placeat?</Button>
+      <Button>Quisquam!</Button>
+      <Button>Dignissimos!</Button>
+      <Button>Accusamus.</Button>
+      <Button>Cupiditate.</Button>
+      <Button>Libero?</Button>
+      <Button>Ratione.</Button>
+      <Button>Sed.</Button>
+      <Button>Odit.</Button>
+      <Button>Expedita.</Button>
+      <Button>Atque!</Button>
+      <Button>Ipsa.</Button>
+      <Button>Cum?</Button>
+      <Button>Totam?</Button>
+      <Button>Sunt.</Button>
     </>
   );
 }

101f38d8126c43a2f023636164c08b03944c505a
jjeongneee <gardening0530@gmail.com>
2023-11-03 12:33:58 +0900

    event.preventDefault():기본기능 취소 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <></>;
+import React from "react";
+import { Button } from "@chakra-ui/react";
+
+function App(props) {
+  function handleClick(e) {
+    // a 태그 클릭시 naver로 이동할 수 있게 메소드 작성
+    e.preventDefault();
+    window.location.href = "http://www.naver.com";
+    console.log("다른 일을 시킴");
+  }
+  return (
+    <div>
+      <a href="http://www.daum.net" onClick={handleClick}>
+        <Button>다음으로 이동</Button>
+      </a>
+    </div>
+  );
 }
 
 export default App;

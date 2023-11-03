f1b9a338e8080c586e8ebd3ff2f2a65bd231938e
jjeongneee <gardening0530@gmail.com>
2023-11-03 15:47:44 +0900

    state 사용해서 dom 그리기 연습시작 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+
+  function num1(e) {
+    setNumber1(Number(e.target.value));
+  }
+
+  function num2(e) {
+    setNumber2(Number(e.target.value));
+  }
+
+  let result = number1 + number2;
+  return (
+    <>
+      <input type="number" onChange={num1} /> +
+      <input type="number" onChange={num2} />= {result}
+    </>
+  );
 }
 
 export default App;

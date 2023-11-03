994d5e5d3f0c7e91e08ee846f28ac2404c7744e0
jjeongneee <gardening0530@gmail.com>
2023-11-03 10:21:16 +0900

    함수를 props으로 전달

--- src/App.js
+++ src/App.js
@@ -1,59 +1,28 @@
 import React from "react";
-import {
-  Box,
-  Button,
-  Flex,
-  Input,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Text,
-} from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Button } from "@chakra-ui/react";
+
+function MyComp({ to, someProp }) {
+  someProp();
+  return (
+    <>
+      <div>
+        <a href={to[0]}>link!!!</a>
+      </div>
+      <div>
+        <Button onClick={someProp}>버튼</Button>
+      </div>
+    </>
+  );
+}
 
 function App(props) {
+  function func1() {
+    console.log("func1 실행됨");
+  }
   return (
-    <Flex
-      px={{ base: "0px", md: "70px", lg: "120px", xl: "200px" }}
-      justify={"space-between"}
-      py={"8px"}
-      my="10px"
-      bg="gray.100"
-    >
-      <Flex gap={"7px"} align={"center"}>
-        <Text as={"b"}>Navbar</Text>
-        <Button variant={"ghost"}>Home</Button>
-        <Button variant={"ghost"}>Link</Button>
-
-        <Menu>
-          <MenuButton
-            variant="ghost"
-            as={Button}
-            rightIcon={<ChevronDownIcon />}
-          >
-            Dropdown
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
-        </Menu>
-
-        <Button variant={"ghost"} isDisabled={true}>
-          Disabled
-        </Button>
-      </Flex>
-      <Flex gap={"7px"} align={"center"}>
-        <Input size={"sm"} bg="white" colorScheme="green" />
-        <Button size={"sm"} variant={"outline"} colorScheme="green">
-          Search
-        </Button>
-      </Flex>
-    </Flex>
+    <div>
+      <MyComp to={["https://www.naver.com"]} someProp={func1}></MyComp>
+    </div>
   );
 }
 

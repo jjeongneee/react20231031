ab45fe41f939e7240f0cc8e547b0b297c37502ed
jjeongneee <gardening0530@gmail.com>
2023-11-03 11:19:18 +0900

    prop으로 함수 전달하는 다양한 방법

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
 
-function MyComp({ children, executeClick }) {
-  return <Button onClick={executeClick}>{children}</Button>;
-}
-
 function App(props) {
-  function func1() {
-    console.log("func1 실행");
+  function handleClickButton1() {
+    console.log("1번 버튼 클릭됨");
   }
 
-  let func2 = () => {
-    console.log("arrow function 실행 111");
-  };
+  function handleClickButton2() {
+    console.log("2번 버튼 클릭됨");
+  }
+
+  function handleButtonClick(number) {
+    console.log(number + "번째 버튼 클릭됨");
+  }
 
   return (
     <div>
-      <MyComp executeClick={func1}>Button1</MyComp>
-      <MyComp executeClick={func2}>Button2</MyComp>
-      <MyComp
-        executeClick={() => {
-          console.log("arrow function 실행 222");
-        }}
-      >
-        Button3
-      </MyComp>
-      <MyComp executeClick={() => console.log("arrow function 실행333")}>
-        Button4
-      </MyComp>
+      <Button onClick={handleClickButton1}>첫번째 버튼</Button>
+      <Button onClick={handleClickButton2}>두번째 버튼</Button>
+      <Button onClick={() => handleButtonClick(3)}>세번째 버튼</Button>
+      <Button onClick={() => handleButtonClick(4)}>네번째 버튼</Button>
     </div>
   );
 }

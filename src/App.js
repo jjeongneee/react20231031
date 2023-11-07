4ea21f6a319e8dbbb8b74f8d11e49a0543565de4
jjeongneee <gardening0530@gmail.com>
2023-11-07 11:16:26 +0900

    다른 컴포넌트의 상태 바꾸기 : 공통 부모에게 상태를 두고 사용해야 함, 연습 완료 -> arrow function으로 변경 완료

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import * as PropTypes from "prop-types";
 import { Box, Input, Text } from "@chakra-ui/react";
 
 function MyInput({ address, onChange }) {
-  function handleInputChange(e) {
-    onChange(e.target.value);
-  }
-
   return (
     <Box>
-      <Input value={address} onChange={handleInputChange} />
+      <Input value={address} onChange={(e) => onChange(e.target.value)} />
     </Box>
   );
 }
@@ -22,16 +18,15 @@ function MyText({ address }) {
   );
 }
 
+// 상태 관리:
+// App 컴포넌트에서는 useState 훅을 사용하여 address라는 상태를 초기화합니다. 이 상태는 현재 입력된 주소를 나타냅니다.
+// handleInputChange 함수는 MyInput 컴포넌트에서 발생한 입력값을 받아와 setAddress를 통해 상태를 업데이트합니다.
 function App(props) {
   const [address, setAddress] = useState("");
 
-  function handleInputChange(text) {
-    setAddress(text);
-  }
-
   return (
     <div>
-      <MyInput address={address} onChange={handleInputChange} />
+      <MyInput address={address} onChange={(text) => setAddress(text)} />
       <MyText address={address} />
     </div>
   );

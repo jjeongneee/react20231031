945253da8b89b8949ef8b0648c5faaa9a6463912
jjeongneee <gardening0530@gmail.com>
2023-11-07 14:39:03 +0900

    다른 파일에 있는 Component가 Context 사용하기 위해서는 Context는 export해야함.

--- src/AComp.js
+++ src/AComp.js
@@ -1,10 +1,10 @@
-import React, {useContext} from "react";
-import {Button} from "@chakra-ui/react";
-import {MessageContext} from "./App";
+import React, { useContext } from "react";
+import { Button } from "@chakra-ui/react";
+import { MessageContext } from "./App";
 
 export function AComp() {
   // 3. context 사용하기 : useContext(Context)
-  const {setMessage} = useContext(MessageContext);
+  const { setMessage } = useContext(MessageContext);
   return (
     <Button onClick={() => setMessage("변경된 메세지")}>
       메시지 변경하기!!!!
